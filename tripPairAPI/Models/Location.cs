8af27a2d9bcd8007df7424c2f395c43e326f3587
Sasa <sasa.trajkova@gmail.com>
2022-07-19 18:24:47 +0200

    Remove cashed files

--- tripPairAPI/Models/Location.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace tripPairAPI.Models;
-
-public class Location
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string GoodMonthsDescription { get; set; }
-}
