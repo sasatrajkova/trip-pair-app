28f203eb6404fd248f9298bb7bbab236ac403d3a
Sasa <sasa.trajkova@gmail.com>
2022-08-02 13:20:38 +0200

    Mondify get calls to eager load data and create mapping to resortDto

--- tripPairAPI/Data/ResortDto.cs
+++ tripPairAPI/Data/ResortDto.cs
@@ -8,4 +8,5 @@ public class ResortDto
     public string Climate { get; set; }
     public string Image { get; set; }
     public int LocationId { get; set; }
+    public LocationDto Location { get; set; }
 }
