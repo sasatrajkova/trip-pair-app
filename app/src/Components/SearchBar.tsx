df7070fdab636528d03cb4b4d898dbcffbeac6b3
Sasa <sasa.trajkova@gmail.com>
2022-08-05 14:49:06 +0200

    Add initial components

---
+++ app/src/Components/SearchBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchBar: React.FC = () => {
+  return <div>This is a search bar.</div>;
+};
+
+export default SearchBar;
---
+++ app/src/Pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "../Components/SearchBar";
+
+const LandingPage: React.FC = () => {
+  return (
+    <div>
+      <SearchBar />
+      Your next advanture awaits.
+    </div>
+  );
+};
+
+export default LandingPage;
