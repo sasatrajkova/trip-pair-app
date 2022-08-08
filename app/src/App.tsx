f4855fe61090f0185c7e8941138c2e399f1d9488
Sasa <sasa.trajkova@gmail.com>
2022-08-08 16:30:09 +0200

    Create intial component and configure routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import LandingPage from "./Pages/LandingPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
+import SearchResultPage from "./Pages/SearchResultPage";
 
 const App: React.FC = () => {
   return (
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/:searchValue" element={<SearchResultPage />} />
         </Routes>
       </Router>
   );
---
+++ app/src/Pages/SearchResultPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../Components/Layout";
+import SearchBar from "../Components/SearchBar";
+
+const SearchResultPage: React.FC = () => {
+    return (
+      <Layout>
+        <SearchBar />
+      </Layout>
+    );
+  };
+
+export default SearchResultPage;
