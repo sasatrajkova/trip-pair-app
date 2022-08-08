0a9c06d35e0dddf29ce84e6a30a4b34cb36c6c2c
Sasa <sasa.trajkova@gmail.com>
2022-08-08 17:26:48 +0200

    Fix search bar

--- app/src/Pages/SearchResultPage.tsx
+++ app/src/Pages/SearchResultPage.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import Layout from "../Components/Layout";
 import SearchBar from "../Components/SearchBar";
 
 const SearchResultPage: React.FC = () => {
+    const { searchValue } = useParams()
+
     return (
       <Layout>
-        <SearchBar />
+        <SearchBar searchValue={searchValue}/>
       </Layout>
     );
   };
