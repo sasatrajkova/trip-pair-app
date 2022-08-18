c7108bce0e87633609c18466940ab6e4469c1399
Sasa <sasa.trajkova@gmail.com>
2022-08-18 16:26:20 +0200

    Small fix

--- api/TripPair.Api/Interfaces/ILocationRepository.cs
+++ api/TripPair.Api/Interfaces/ILocationRepository.cs
@@ -7,7 +7,7 @@ public interface ILocationRepository
     Task<IEnumerable<Location>> GetAllLocations();
     Task<Location?> GetLocationById(int locationId);
     Task<Location?> GetLocationByName(string locationName);
-    Task<IEnumerable<Month?>> GetLocationMonths(int locationId);
+    Task<IEnumerable<Month>> GetLocationMonths(int locationId);
     Task<Location?> UpdateLocation(int id, Location locationToUpdate);
     Task<Location> CreateLocation(Location newLocation);
     Task<Location?> DeleteLocation(int locationId);
