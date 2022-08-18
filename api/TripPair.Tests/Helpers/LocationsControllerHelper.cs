285aeb77e2b41405b06211e876436d10c23c2319
Sasa <sasa.trajkova@gmail.com>
2022-08-18 14:51:12 +0200

    Extend location helper

--- api/TripPair.Tests/Helpers/LocationsControllerHelper.cs
+++ api/TripPair.Tests/Helpers/LocationsControllerHelper.cs
@@ -8,14 +8,22 @@ public static class LocationsControllerHelper
 {
     public static IEnumerable<Location> ListOfLocations()
     {
-        var location1 = CreateLocation(new Random().Next(), "location1", "month1");
+        var location1 = CreateLocation(1, "location1", "month1");
         var location2 = CreateLocation(new Random().Next(), "location1", "month1");
         var location3 = CreateLocation(new Random().Next(), "location2", "month2");
 
         return new List<Location> {location1, location2, location3};
     }
+    
+    public static IEnumerable<LocationMonth> ListOfLocationMonths()
+    {
+        var locationMonth1 = CreateLocationMonth(1,  1);
+        var locationMonth2 = CreateLocationMonth(new Random().Next(), 2);
 
-    private static Location CreateLocation(int id, string locationName, string month)
+        return new List<LocationMonth> {locationMonth1, locationMonth2};
+    }
+
+    public static Location CreateLocation(int id, string locationName, string month)
     {
         return new Location
         {
@@ -30,6 +38,23 @@ public static class LocationsControllerHelper
                         Name = month
                     }
                 }
+            }
+        };
+    }
+
+    private static LocationMonth CreateLocationMonth(int locationId, int monthId)
+    {
+        return new LocationMonth
+        {
+            Id = new Random().Next(),
+            LocationId = locationId,
+            Location = new Location(),
+            MonthId = monthId,
+            Month = new Month
+            {
+                Id = monthId,
+                MonthLocations = new List<LocationMonth>(),
+                Name = Guid.NewGuid().ToString()
             }
         };
     }
