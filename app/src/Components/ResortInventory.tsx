05e8ac4ce15d2fe343154af2d04db96986a2e2a3
Sasa <sasa.trajkova@gmail.com>
2022-08-29 13:08:21 +0200

    Configure responsive gird

--- app/src/Components/ResortInventory.tsx
+++ app/src/Components/ResortInventory.tsx
@@ -3,9 +3,9 @@ import ResortCard from "./ResortCard";
 
 const ResortInventory: React.FC = () => {
   return (
-    <div className="grid grid-cols-3 gap-8 px-8 pt-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 px-8 pt-8">
       {[1, 2, 3, 4, 5, 6].map((i) => (
-        <ResortCard />
+        <ResortCard key={i}/>
       ))}
     </div>
   );
